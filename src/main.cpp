41327e1460617c5653a78673974cf9fd0e22a1b3
a64070ho <harry.obrien@student.manchester.ac.uk>
2020-11-08 15:32:30 +0000

    Create initial io tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+#ifndef UNIT_TEST
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
+#endif /* ifndef UNIT_TEST */
---
+++ test/Mbed_IO_Test/Mbed_IO_Test.cpp
@@ -0,0 +1,17 @@
+//
+//  Mbed_IO_Test.cpp
+//  Hyperloop_Motor_Control_Unit
+//
+//  Created by Harry O'Brien on 08/11/2020.
+//  Copyright © 2020 Hyperloop Manchester. All rights reserved.
+//
+
+#include <unity.h>
+
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    UNITY_END();
+    return 0;
+}
